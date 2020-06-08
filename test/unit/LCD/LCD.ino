983b3646081ac29ad8888198aadd94727e841e40
Ruangrith Rithruangdech <ruangrith.ri@gmail.com>
2020-06-08 09:22:20 +0700

    test

---
+++ test/unit/LCD/LCD.ino
@@ -0,0 +1,30 @@
+#include "LiquidCrystal_I2C.h"
+
+// Set the LCD address to 0x27 for a 16 chars and 2 line display
+LiquidCrystal_I2C lcd(0x27, 16, 2);
+
+void setup() {
+  lcd.init();
+  lcd.backlight();
+
+  lcd.setCursor(0, 0);
+  lcd.print("Hello, world!");
+
+  lcd.setCursor(0, 1);
+  lcd.print("Hi");
+
+  delay(2000);
+
+  lcd.clear();
+}
+
+void loop() {
+  lcd.setCursor(0, 0);
+  // lcd.print("Hello, world!");
+
+  lcd.setCursor(0, 1);
+  lcd.print(millis());
+
+  delay(20);
+  lcd.clear();
+}
