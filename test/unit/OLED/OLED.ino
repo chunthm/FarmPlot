4572bb1cce59ad5a1cf65977076af870b9716354
Chanthima Pinkaew <chunthima15@gmail.com>
2020-06-15 17:37:57 +0700

    add oled and temp code

--- test/unit/OLED/OLED.ino
+++ test/unit/OLED/OLED.ino
@@ -1,69 +1,24 @@
-/*
-
-INSTRALL FORM IDE LIBRARY MANAGER
-- Adafruit IO
-- Adafruit GFX
-- Adafruit SSD1306
-
-*/
-
-#include <Adafruit_GFX.h>
-#include <Adafruit_SSD1306.h>
 #include <SPI.h>
 #include <Wire.h>
-
-Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
-
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+ 
+#define SCREEN_WIDTH 128 
+#define SCREEN_HEIGHT 64 
+ 
+#define OLED_RESET -1 
+Adafruit_SSD1306 OLED(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
+ 
 void setup() {
-  pinMode(2, INPUT_PULLUP);
-
-  oled.begin(0x3C); // Address 0x3C for 128x32
-
-  oled.clearDisplay();
-
-  oled.setCursor(0, 0);
-
-  oled.setTextColor(SSD1306_WHITE);
-  oled.setTextSize(1);
-  oled.print("text 1");
-
-  oled.setTextSize(2);
-  oled.println("text 2");
-
-  oled.setTextSize(3);
-  oled.println("text 3");
-
-  oled.setTextSize(1);
-  oled.println("Hello World");
-
-  oled.display();
-
-  delay(2000);
+Serial.begin(9600);
 }
-
-int i = 0;
-bool flip = false;
-
+ 
 void loop() {
-  oled.clearDisplay();
-  oled.setCursor(0, 0);
-
-  if (!digitalRead(2)) {
-    oled.println("HI 0,0");
-  }
-
-  oled.setCursor(64, 16);
-  oled.println("HI 64, 16");
-  oled.println(millis());
-
-  oled.drawLine(0, 0, 32, 32, SSD1306_WHITE);
-
-  oled.drawCircle(0, 0, 20, SSD1306_WHITE);
-  oled.drawRect(32, 0, 20, 20, SSD1306_WHITE);
-
-  oled.fillCircle(64, 0, i, SSD1306_WHITE);
-  oled.fillRect(96, 0, 20, 10, SSD1306_WHITE);
-
-  oled.display();
-  delay(10);
+OLED.clearDisplay();
+OLED.setTextColor(WHITE, BLACK);
+OLED.setCursor(10,35);
+OLED.setTextSize(2);
+OLED.println("_CHUNTHM_");
+OLED.display();
+delay(1000);
 }
